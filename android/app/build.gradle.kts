09fbd44556ca30b4269793fdda05dbc8d3a62d84
arundev43 <arundev4374@gmail.com>
2026-09-07 21:55:20 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,10 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.14.0"))
-    implementation("com.google.firebase:firebase-analytics")
-
     // Required for flutter_local_notifications
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
@@ -17,7 +13,6 @@ android {
     namespace = "com.example.rtoexpert"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
     setProperty("archivesBaseName", "rtoexpert")
 
     compileOptions {
